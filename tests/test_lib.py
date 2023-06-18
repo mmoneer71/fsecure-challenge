c1e5a4114980aaadd396418393002b0110f402d7
mmoneer71 <moneer.m71@gmail.com>
2023-06-18 18:51:43 +0300

    more tests

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,7 +1,7 @@
 from batchlib.utils import MAX_RECORD_SIZE, create_output_records
 
 
-def test_basic_case():
+def test_basic_case_var_size():
     sample_input = ["aaaaaaaaaaaaaaaaaa", "bbbbbbbbbbbbbbbbb", "ccccccccccccc"]
     output = create_output_records(sample_input)
     assert output == [["aaaaaaaaaaaaaaaaaa", "bbbbbbbbbbbbbbbbb", "ccccccccccccc"]]
@@ -12,7 +12,7 @@ def test_empty_input():
     assert output == []
 
 
-def test_record_gt_1mb():
+def test_record_gt_1mb_var_size():
     sample_input = [
         "aaaaaaaaaaaaaaaaaa" * MAX_RECORD_SIZE,
         "bbbbbbbbbbbbbbbbb",
@@ -62,3 +62,54 @@ def test_max_500_record():
         ["aaaaaaaaaaaaaaaaaa", "bbbbbbbbbbbbbbbbb"] * 50
         + ["ccccccccccccc", "ddddddddddddd"] * 200,
     ]
+
+def test_all_records_invalid():
+    sample_input = [
+        "aaaaaaaaaaaaaaaaaa" * MAX_RECORD_SIZE,
+        "bbbbbbbbbbbbbbbbb" * MAX_RECORD_SIZE,
+        "ccccccccccccc" * MAX_RECORD_SIZE,
+    ] * MAX_RECORD_SIZE
+    output = create_output_records(sample_input)
+    assert output == []
+
+def test_large_input():
+    sample_input = [
+            "a" * MAX_RECORD_SIZE,
+            "b" * MAX_RECORD_SIZE,
+            "c" * MAX_RECORD_SIZE,
+            "d" * MAX_RECORD_SIZE,
+            "e" * MAX_RECORD_SIZE,
+        ] * 1000
+    output = create_output_records(sample_input)
+    expected_single_batch = [
+            "a" * MAX_RECORD_SIZE,
+            "b" * MAX_RECORD_SIZE,
+            "c" * MAX_RECORD_SIZE,
+            "d" * MAX_RECORD_SIZE,
+            "e" * MAX_RECORD_SIZE,
+        ]
+    expected_output = []
+    for _ in range(0, 1000):
+        expected_output.append(expected_single_batch)
+    assert output == expected_output
+
+def test_even_larger_input():
+    sample_input = [
+            "a" * MAX_RECORD_SIZE,
+            "b" * MAX_RECORD_SIZE,
+            "c" * MAX_RECORD_SIZE,
+            "d" * MAX_RECORD_SIZE,
+            "e" * MAX_RECORD_SIZE,
+        ] * 10000
+    output = create_output_records(sample_input)
+    expected_single_batch = [
+            "a" * MAX_RECORD_SIZE,
+            "b" * MAX_RECORD_SIZE,
+            "c" * MAX_RECORD_SIZE,
+            "d" * MAX_RECORD_SIZE,
+            "e" * MAX_RECORD_SIZE,
+        ]
+    expected_output = []
+    for _ in range(0, 10000):
+        expected_output.append(expected_single_batch)
+    assert output == expected_output
